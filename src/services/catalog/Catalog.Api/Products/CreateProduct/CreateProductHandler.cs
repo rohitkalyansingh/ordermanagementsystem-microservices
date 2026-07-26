c84be6023f825b739e78024f8a648d44f0522930
Kalyani singh <kalyani.singh@nomiso.io>
2026-07-26 12:48:40 +0530

    Created a Product Handler for product catalog

---
+++ src/services/catalog/Catalog.Api/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.Api.Products.CreateProduct
+{
+
+    public record CreateProductCommand(string name, List<string> Category, string description) : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid id);
+
+    public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
